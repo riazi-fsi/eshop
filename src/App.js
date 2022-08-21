99d44b5bfbde611ee25826a735321aa485db9240
F_Riazi <f_riazi@isc.co.ir>
2022-08-21 13:34:06 +0430

    add router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import Header from './Header/Header'
 import Home from './Home/Home'
-
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom'
+import Checkout from './Checkout/Checkout'
 function App() {
   return (
     <div>
+      <Router>
+        <Routes>
+          <Route path='/checkout' element={<Checkout/>} />
+          <Route path='/'  element={<Home/>}/>
+            
+        </Routes>
+      </Router>
 
-      <Header/>
-      <Home/>
     </div>
   )
 }
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,13 +3,16 @@ import './Header.css';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 import StoreFrontIcon from '@material-ui/icons/Storefront';
 import SearchIcon from '@material-ui/icons/Search';
+import { Link } from 'react-router-dom';
 export default function Header() {
     return (
         <div className='header'>
+            <Link to="/" style={{ textDecoration: 'none' }}>
             <div className='header__logo'>
                 <StoreFrontIcon className='header__logoImage' fontSize='large' />
                 <h2 className="header__logoTitle">eshop</h2>
             </div>
+            </Link>
             <div className='header__search'>
                 <input type="text" className='header__serachInput' />
                 <SearchIcon className='header__searchIcon' />
@@ -24,10 +27,13 @@ export default function Header() {
                     <span className="nav__itemLineOne">your</span>
                     <span className="nav__itemLineTwo">shop</span>
                 </div>
-                <div  className='nav__itemBasket'>
-                    <ShoppingBasketIcon fontSize='large' />
-                    <span className="nav__itemLineTwo nav__basketCount">0</span>
-                </div>
+                <Link to="/checkout" style={{ textDecoration: 'none' }}>
+                    <div className='nav__itemBasket'>
+                        <ShoppingBasketIcon fontSize='large' />
+                        <span className="nav__itemLineTwo nav__basketCount">0</span>
+                    </div>
+                </Link>
+
             </div>
 
         </div>
