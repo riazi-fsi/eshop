0e0de98438279d621638b75d7e1ba4ab00b223b0
F_Riazi <f_riazi@isc.co.ir>
2022-08-22 16:18:17 +0430

    responsive

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -9,7 +9,8 @@ export default function Header() {
     const [{ basket }, dispatch] = useStateValue();
 
     return (
-        <div className='flex items-center bg-slate-900 h-12 sticky z-50 top-0'>
+        <>
+        <div className='flex items-center justify-between bg-slate-900 h-12 sticky z-50 top-0'>
             <Link to="/" style={{ textDecoration: 'none' }}>
                 <div className=' flex items-center text-orange-400'>
                     <StoreFrontIcon className=' mx-2 my-0 text-orange-400 flex items-center' fontSize='large' />
@@ -17,10 +18,10 @@ export default function Header() {
                 </div>
             </Link>
             <div className=' flex flex-1 items-center mx-8 '>
-                <input type="text" className=' h-3 p-3 bottom-0 w-full' />
+                <input type="text" className=' h-3 p-3 bottom-0 w-[200px]' />
                 <SearchIcon className=' p-1 h-2 bg-yellow-400' />
             </div>
-            <div className="flex content-evenly ">
+            <div className="hidden sm:flex content-evenly  ">
                 <div className=" flex flex-col mx-4 text-white">
                     <span className="text-xs">Hello Guest</span>
                     <span className="text-sm font-bold">Sign Two</span>
@@ -38,7 +39,26 @@ export default function Header() {
                 </Link>
 
             </div>
+            
+        </div>
+        <div className='flex items-center justify-between bg-slate-900 h-12 sticky z-50 top-0 sm:hidden'>
+        <div className=" flex flex-col mx-4 text-white">
+                    <span className="text-xs">Hello Guest</span>
+                    <span className="text-sm font-bold">Sign Two</span>
+
+                </div>
+                <div className="flex flex-col mx-4 text-white">
+                    <span className="text-xs">your</span>
+                    <span className="text-sm font-bold">shop</span>
+                </div>
+        <Link to="/checkout" style={{ textDecoration: 'none' }}>
+                    <div className='flex items-center text-white mr-4 ml-2'>
+                        <ShoppingBasketIcon fontSize='large' />
+                        <span className=" font-bold text-sm mx-2">{basket.length}</span>
+                    </div>
+                </Link>
 
         </div>
+        </>
     )
 }
