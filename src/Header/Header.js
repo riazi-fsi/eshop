52fe1d62f4543e179d2149549e24041d764cf93c
F_Riazi <f_riazi@isc.co.ir>
2022-08-23 09:58:38 +0430

    add login page

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -10,18 +10,39 @@ export default function Header() {
 
     return (
         <>
-        <div className='flex items-center justify-between bg-slate-900 h-12 sticky z-50 top-0'>
-            <Link to="/" style={{ textDecoration: 'none' }}>
-                <div className=' flex items-center text-orange-400'>
-                    <StoreFrontIcon className=' mx-2 my-0 text-orange-400 flex items-center' fontSize='large' />
-                    <h2 className="text-white border-none decoration-0">eshop</h2>
+            <div className='flex items-center justify-between bg-slate-900 h-12 sticky z-50 top-0'>
+                <Link to="/" style={{ textDecoration: 'none' }}>
+                    <div className=' flex items-center text-orange-400'>
+                        <StoreFrontIcon className=' mx-2 my-0 text-orange-400 flex items-center' fontSize='large' />
+                        <h2 className="text-white border-none decoration-0">eshop</h2>
+                    </div>
+                </Link>
+                <div className=' flex flex-1 items-center mx-8 '>
+                    <input type="text" className=' h-3 p-3 bottom-0 w-[200px]' />
+                    <SearchIcon className=' p-1 h-2 bg-yellow-400' />
                 </div>
-            </Link>
-            <div className=' flex flex-1 items-center mx-8 '>
-                <input type="text" className=' h-3 p-3 bottom-0 w-[200px]' />
-                <SearchIcon className=' p-1 h-2 bg-yellow-400' />
+                <div className="hidden sm:flex content-evenly  ">
+                    <div className=" flex flex-col mx-4 text-white">
+                        <span className="text-xs">Hello Guest</span>
+                        <Link to="/login" style={{ textDecoration: 'none' }}>
+                            <span className="text-sm font-bold">Sign Two</span>
+                        </Link>
+                    </div>
+                    <div className="flex flex-col mx-4 text-white">
+                        <span className="text-xs">your</span>
+                        <span className="text-sm font-bold">shop</span>
+                    </div>
+                    <Link to="/checkout" style={{ textDecoration: 'none' }}>
+                        <div className='flex items-center text-white mr-4 ml-2'>
+                            <ShoppingBasketIcon fontSize='large' />
+                            <span className=" font-bold text-sm mx-2">{basket.length}</span>
+                        </div>
+                    </Link>
+
+                </div>
+
             </div>
-            <div className="hidden sm:flex content-evenly  ">
+            <div className='flex items-center justify-between bg-slate-900 h-12 sticky z-50 top-0 sm:hidden'>
                 <div className=" flex flex-col mx-4 text-white">
                     <span className="text-xs">Hello Guest</span>
                     <span className="text-sm font-bold">Sign Two</span>
@@ -39,26 +60,6 @@ export default function Header() {
                 </Link>
 
             </div>
-            
-        </div>
-        <div className='flex items-center justify-between bg-slate-900 h-12 sticky z-50 top-0 sm:hidden'>
-        <div className=" flex flex-col mx-4 text-white">
-                    <span className="text-xs">Hello Guest</span>
-                    <span className="text-sm font-bold">Sign Two</span>
-
-                </div>
-                <div className="flex flex-col mx-4 text-white">
-                    <span className="text-xs">your</span>
-                    <span className="text-sm font-bold">shop</span>
-                </div>
-        <Link to="/checkout" style={{ textDecoration: 'none' }}>
-                    <div className='flex items-center text-white mr-4 ml-2'>
-                        <ShoppingBasketIcon fontSize='large' />
-                        <span className=" font-bold text-sm mx-2">{basket.length}</span>
-                    </div>
-                </Link>
-
-        </div>
         </>
     )
 }
